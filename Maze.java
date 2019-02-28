2cce22c183addb3ce5c4c7464f71b7a7074ee468
Oliver Ripps <oripps@stuy.edu>
2019-02-28 10:44:35 -0500

    added solve method draft

--- Maze.java
+++ Maze.java
@@ -83,66 +83,7 @@ public class Maze{
         //erase terminal, go to top left of screen.
 
         System.out.println("\033[2J\033[1;1H");
-
-    }import java.util.*;
-import java.io.*;
-public class Maze{
-
-
-    private char[][]maze;
-    private boolean animate;//false by default
-
-    /*Constructor loads a maze text file, and sets animate to false by default.
-
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - Walls - locations that cannot be moved onto
-      ' ' - Empty Space - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-
-      'S' - the location of the start(exactly 1 per file)
-
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-
-
-      3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
-
-         throw a FileNotFoundException or IllegalStateException
-
-    */
-
-    public Maze(String filename) throws FileNotFoundException{
-        //COMPLETE CONSTRUCTOR
-    }
-
-
-    private void wait(int millis){
-         try {
-             Thread.sleep(millis);
-         }
-         catch (InterruptedException e) {
-         }
-     }
-
-
-    public void setAnimate(boolean b){
-
-        animate = b;
-
-    }
-
-
-    public void clearTerminal(){
-
-        //erase terminal, go to top left of screen.
-
-        System.out.println("\033[2J\033[1;1H");
-
-    }
-
-
-
-
-
+      }
 
    /*Return the string that represents the maze.
 
@@ -239,16 +180,19 @@ public class Maze{
 
     */
     public int solve(){
+      int srow;
+      int scol;
 
-            //find the location of the S.
-
-
-            //erase the S
-
-
-            //and start solving at the location of the s.
-
-            //return solve(???,???);
+      for(int i=0;i<maze.length;i++){
+        for(int l=0;l<maze[0].length;l++){
+          if(maze[i][l]=='S'){
+            srow=i;
+            scol=l;
+            maze[i][l]=0;
+          }
+        }
+      }
+      solve(srow,scol);
 
     }
 
