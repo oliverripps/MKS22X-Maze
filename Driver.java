e75f0f44d723ad3af5bed396b6578b8b88fdb9b1
Oliver Ripps <oripps@stuy.edu>
2019-03-03 15:09:50 -0500

    added test case to driver

--- Driver.java
+++ Driver.java
@@ -1,8 +1,16 @@
 public class Driver{
 
   public static void main(String[] args){
+    String filename=args[0];
+    try{
+      Maze f= new Maze(filename);
+      f.solve();
+      System.out.println(toString(f));
+    }
+    catch(FileNotFoundException e){
+      System.out.println("File not found: " + filename);
+    }
 
-    
   }
 
 
